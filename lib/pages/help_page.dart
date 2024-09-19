fba41fac3abc6ef6affbf7bb51cb8268e5bfb2f5
Mukhammad Akilov <mukhamadkhon_akilov@epam.com>
2024-09-19 10:47:13 +0500

    Add: ability lo show PDF files

--- lib/pages/help_page.dart
+++ lib/pages/help_page.dart
@@ -1,5 +1,18 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lows_app/pages/pdf-screen.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 
 class HelpPage extends StatefulWidget {
   const HelpPage({super.key});
@@ -10,12 +23,47 @@ class HelpPage extends StatefulWidget {
 
 class _HelpPageState extends State<HelpPage> {
   bool _showPhoneNumbers = false;
+  late String pdf1;
+  late String pdf2;
+
+  Future<File> fromAsset(String asset, String filename) async {
+    // To open from assets, you can copy them to the app storage folder, and the access them "locally"
+    Completer<File> completer = Completer();
+
+    try {
+      var dir = await getApplicationDocumentsDirectory();
+      File file = File("${dir.path}/$filename");
+      var data = await rootBundle.load(asset);
+      var bytes = data.buffer.asUint8List();
+      await file.writeAsBytes(bytes, flush: true);
+      completer.complete(file);
+    } catch (e) {
+      throw Exception('Error parsing asset file!');
+    }
+
+    return completer.future;
+  }
+
+  @override
+  void initState() {
+    fromAsset('assets/file-1.pdf', 'file-1.pdf').then((f) {
+      setState(() {
+        pdf1 = f.path;
+      });
+    });
+    fromAsset('assets/file-2.pdf', 'file-2.pdf').then((f) {
+      setState(() {
+        pdf2 = f.path;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
+        backgroundColor: Theme.of(context).colorScheme.surface,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -113,6 +161,33 @@ class _HelpPageState extends State<HelpPage> {
                 end: 0,
                 duration: Duration(milliseconds: 250),
               ),
+          //EXAMPLE how to open PDF
+          TextButton(
+            child: Text("Open PDF 1"),
+            onPressed: () {
+              if (pdf1.isNotEmpty) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PDFScreen(path: pdf1),
+                  ),
+                );
+              }
+            },
+          ),
+          TextButton(
+            child: Text("Open PDF 2"),
+            onPressed: () {
+              if (pdf1.isNotEmpty) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PDFScreen(path: pdf2),
+                  ),
+                );
+              }
+            },
+          ),
         ],
       ),
     );
